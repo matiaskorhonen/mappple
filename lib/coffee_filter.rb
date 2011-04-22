8b334f9e7d8f534599516dc5a5885bdc6653c0f3
Matias Korhonen <matias@kiskolabs.com>
2011-04-22 12:59:17 +0300

    Use the coffee-script rubygem for CS compiling.

--- lib/coffee_filter.rb
+++ lib/coffee_filter.rb
@@ -1,25 +1,9 @@
-require "open3"
-require "win32/open3" if RUBY_PLATFORM.match /win32/
+require "coffee_script"
 
 class CoffeeFilter < Nanoc3::Filter
   identifier :coffee
 
   def run(content, params = {})
-    output = ""
-    error = ""
-    command = "coffee -s -p -l"
-    Open3.popen3(command) do |stdin, stdout, stderr|
-      stdin.puts content
-      stdin.close
-      output = stdout.read.strip
-      error = stderr.read.strip
-      [stdout, stderr].each { |io| io.close }
-    end
-
-    if error.length > 0
-      raise("Compilation error:\n#{error}")
-    else
-      output
-    end
+    CoffeeScript.compile(content)
   end
 end
